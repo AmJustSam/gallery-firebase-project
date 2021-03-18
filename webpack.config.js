2cfad3fce3c05476ed4960dbcf661d7445b600d9
Usama Tahir <amjustsam@protonmail.com>
2021-03-18 22:22:56 +0500

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -28,32 +28,32 @@ module.exports = {
      }),
   ],
 
-  // optimization: {
-  //   runtimeChunk: 'single',
-  //   splitChunks: {
-  //     chunks: 'all',
-  //     maxInitialRequests: Infinity,
-  //     minSize: 0,
-  //     cacheGroups: {
-  //       vendor: {
-  //         test: /[\\/]node_modules[\\/]/,                       // will match exactly this --> "/node_modules/"
+  optimization: {
+    runtimeChunk: 'single',
+    splitChunks: {
+      chunks: 'all',
+      maxInitialRequests: Infinity,
+      minSize: 0,
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,                       // will match exactly this --> "/node_modules/"
       
-  //         // Normally, you would just define a name for the output file as a string.
-  //         // But I’m defining name as a function (which will be called for every parsed file)
-  //         // As a result, we’ll get one file for each package, e.g. npm.react-dom.899sadfhj4.js
+          // Normally, you would just define a name for the output file as a string.
+          // But here I'm defining name as a function (which will be called for every parsed file)
+          // As a result, we’ll get one file for each package, e.g. npm.react-dom.899sadfhj4.js
 
-  //         name(module) {
+          name(module) {
 
-  //           // get the name. E.g. node_modules/packageName/not/this/part.js
-  //           // or node_modules/packageName
+            // get the name. E.g. node_modules/packageName/not/this/part.js
+            // or node_modules/packageName
 
-  //           const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+            const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
 
-  //           // npm package names are URL-safe, but some servers don't like @ symbols
-  //           return `npm.${packageName.replace('@', '')}`;
-  //         },
-  //       },
-  //     },
-  //   },
-  // }
+            // npm package names are URL-safe, but some servers don't like @ symbols
+            return `npm.${packageName.replace('@', '')}`;
+          },
+        },
+      },
+    },
+  }
 }
